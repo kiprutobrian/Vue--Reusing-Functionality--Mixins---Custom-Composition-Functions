96e026c7ac37876a12b4f4439230a14a1f2952b6
kiprutobrian <briankipruto793@gmail.com>
2021-02-04 05:20:29 +0300

    reuse-02-using-mixins

--- src/components/DeleteUser.vue
+++ src/components/DeleteUser.vue
@@ -11,22 +11,12 @@
 <script>
 import UserAlert from './UserAlert.vue';
 
+import alertMixin from '../mixins/alert.js';
+
 export default {
   components: {
     UserAlert,
   },
-  data() {
-    return {
-      alertIsVisible: false,
-    };
-  },
-  methods: {
-    showAlert() {
-      this.alertIsVisible = true;
-    },
-    hideAlert() {
-      this.alertIsVisible = false;
-    },
-  },
+  mixins: [alertMixin],
 };
 </script>
