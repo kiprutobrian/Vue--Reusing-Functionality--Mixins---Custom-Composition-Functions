c4e558e5e24ba3d656f2cb0d58499d73ebdc297e
kiprutobrian <briankipruto793@gmail.com>
2021-02-04 05:26:47 +0300

    reuse-09-example-a-custom-sort-hook

--- src/components/users/UserList.vue
+++ src/components/users/UserList.vue
@@ -19,10 +19,11 @@
 </template>
 
 <script>
-import { ref, computed, toRefs } from 'vue';
+import { toRefs } from 'vue';
 
 import UserItem from './UserItem.vue';
 import useSearch from '../../hooks/search.js';
+import useSort from '../../hooks/sort.js';
 
 export default {
   components: {
@@ -38,27 +39,10 @@ export default {
       'fullName'
     );
 
-    const sorting = ref(null);
-    const displayedUsers = computed(function () {
-      if (!sorting.value) {
-        return availableItems.value;
-      }
-      return availableItems.value.slice().sort((u1, u2) => {
-        if (sorting.value === 'asc' && u1.fullName > u2.fullName) {
-          return 1;
-        } else if (sorting.value === 'asc') {
-          return -1;
-        } else if (sorting.value === 'desc' && u1.fullName > u2.fullName) {
-          return -1;
-        } else {
-          return 1;
-        }
-      });
-    });
-
-    function sort(mode) {
-      sorting.value = mode;
-    }
+    const { sorting, displayedUsers, sort } = useSort(
+      availableItems,
+      'fullName'
+    );
 
     return {
       enteredSearchTerm,
